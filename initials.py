6b7745e8b3007f8cd0b2a4ce4afa89e86499f819
Amankwah Lois Nana Ekua <amankwahlois041@gmail.com>
2026-05-30 20:55:53 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,32 @@
+    # File: initials.py
+    # Description: Print out my initials.
+    # Assignment Number: 1
+    #
+    # Name: Amankwah Lois Nana Ekua
+    # STUDENT ID: 2425402335
+    # Email: 2425402335@live.gctu.edu.gh
+    # Grader: Mr. Augustus Buckman
+    #
+    # On my honor, Amankwah Lois Nana Ekua, this programming assignment is my own work
+    # and I have not provided this code to any other student.
 
 def main():
-  pass #code goes here
 
+        print()
+        print("...AEL")
+        print()
+
+        print("......AAAA.............EEEEEEEEEEEE........LL...............")
+        print(".....AA...AA...........EEE.................LL...............")
+        print("...AA.......AA.........EEE.................LL...............")
+        print("...AAA......AAA........EEE.................LL...............")
+        print("...AAAAAAAAAAAA........EEEEEEEEEEEE........LL...............")
+        print("...AA........AA........EEE.................LL...............")
+        print("...AA........AA........EEE.................LL...............")
+        print("...AA........AA........EEE.................LL...............")
+        print("...AA........AA...**...EEE............**...LL.............**")
+        print("...AA........AA...**...EEEEEEEEEEEE...**...LLLLLLLLLLLL...**")
+        print()
 
 
 main()
